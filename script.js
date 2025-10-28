baffe715f0f23959fe53a97015b14589c6287f83
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-10-28 15:14:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// DATI
+// tutti i numeri da 1 a 100
+// ESECUZIONE LOGICA
+// 1 creo un ciclo for che mi stampa tutti i numeri da 1 a 100
+// 2 creo un if in cui tutti i numneri divisibili per 3 si chiamano Fizz
+// 3 creo un else if in cui tutti i numeri divisibili per 5 si chiamano buzz
+// 4 altro else if in cui i numeri divisibili sia per 3 che per 5 si chiamano fizzbuzz 
