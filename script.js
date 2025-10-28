b64ee9843c704c5e3eff68503aa69f0b4173a904
gennaroderosa10 <gennaroderosa5@gmail.com>
2025-10-28 15:30:17 +0100

    feat: done

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 // tutti i numeri da 1 a 100
 // ESECUZIONE LOGICA
 // 1 creo un ciclo for che mi stampa tutti i numeri da 1 a 100
-// 2 creo un if in cui tutti i numneri divisibili per 3 si chiamano Fizz
-// 3 creo un else if in cui tutti i numeri divisibili per 5 si chiamano buzz
-// 4 altro else if in cui i numeri divisibili sia per 3 che per 5 si chiamano fizzbuzz 
+// 2 altro else if in cui i numeri divisibili sia per 3 che per 5 si chiamano fizzbuzz
+// 3 creo un if in cui tutti i numneri divisibili per 3 si chiamano Fizz
+// 4 creo un else if in cui tutti i numeri divisibili per 5 si chiamano buzz
+//  
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0) {
+        console.log("Fizz")
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else (console.log(i))
+
+} 
